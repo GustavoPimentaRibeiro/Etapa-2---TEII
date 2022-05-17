d2e93d528130e6653e8da7035d2fa4ec33f73e85
Gustavo Pimenta Ribeiro <gustavo.pimenta.ribeiro@gmail.com>
2022-05-17 11:59:07 -0300

    Primeiro commit

---
+++ src/main/java/ifsuldeminas/pas/bcc/teii/Etapa2/Etapa2Application.java
@@ -0,0 +1,13 @@
+package ifsuldeminas.pas.bcc.teii.Etapa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Etapa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Etapa2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ifsuldeminas/pas/bcc/teii/Etapa2/Etapa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsuldeminas.pas.bcc.teii.Etapa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Etapa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
