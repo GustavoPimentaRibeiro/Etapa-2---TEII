7ec6d6507753399116f001273048730942f081ce
carlosantjrr <carlosantjrr@gmail.com>
2022-07-06 20:42:52 -0300

    Antonio Carlos

--- src/main/java/ifsuldeminas/pas/bcc/teii/Etapa2/model/entity/Leilao.java
+++ src/main/java/ifsuldeminas/pas/bcc/teii/Etapa2/model/entity/Leilao.java
@@ -2,10 +2,64 @@ package ifsuldeminas.pas.bcc.teii.Etapa2.model.entity;
 
 import java.util.Date;
 import java.util.Set;
+import javax.persistence.*;
 
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="tipo_venda", discriminatorType = DiscriminatorType.INTEGER)
 public class Leilao {
-    private long destinatario_id;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+    @OneToMany
+    @JoinColumn(name="usuario_id")
     private Set<Item> item_id_qtdd;
+    private long destinatario_id;
     private long remetente_id;
+    @Temporal(TemporalType.TIMESTAMP)
     private Date tempo_item_leilao;
+
+    public Leilao(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Set<Item> getItem_id_qtdd() {
+        return item_id_qtdd;
+    }
+
+    public void setItem_id_qtdd(Set<Item> item_id_qtdd) {
+        this.item_id_qtdd = item_id_qtdd;
+    }
+
+    public long getDestinatario_id() {
+        return destinatario_id;
+    }
+
+    public void setDestinatario_id(long destinatario_id) {
+        this.destinatario_id = destinatario_id;
+    }
+
+    public long getRemetente_id() {
+        return remetente_id;
+    }
+
+    public void setRemetente_id(long remetente_id) {
+        this.remetente_id = remetente_id;
+    }
+
+    public Date getTempo_item_leilao() {
+        return tempo_item_leilao;
+    }
+
+    public void setTempo_item_leilao(Date tempo_item_leilao) {
+        this.tempo_item_leilao = tempo_item_leilao;
+    }
 }
--- src/main/java/ifsuldeminas/pas/bcc/teii/Etapa2/model/entity/Usuario.java
+++ src/main/java/ifsuldeminas/pas/bcc/teii/Etapa2/model/entity/Usuario.java
@@ -1,11 +1,17 @@
 package ifsuldeminas.pas.bcc.teii.Etapa2.model.entity;
 
 import java.util.Set;
+import javax.persistence.*;
 
+@Entity
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
+    @OneToMany
+    @JoinColumn(name="usario_id")
+    private Set<Item> itens;
     private String usuario;
     private String senha;
     private String email;
-    private Set<Item> itens;
 }
