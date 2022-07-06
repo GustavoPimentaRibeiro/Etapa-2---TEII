ee82758745f46967b48cac8cdb9700eafab75d71
Andre Chaves <andre.chaves@alunos.ifsuldeminas.edu.br>
2022-07-06 20:56:53 -0300

    Andre Luiz

--- src/main/java/ifsuldeminas/pas/bcc/teii/Etapa2/model/entity/VendaCorrente.java
+++ src/main/java/ifsuldeminas/pas/bcc/teii/Etapa2/model/entity/VendaCorrente.java
@@ -1,6 +1,11 @@
 package ifsuldeminas.pas.bcc.teii.Etapa2.model.entity;
+import javax.persistence.*;
 
+@Entity
+@DiscriminatorValue("1")
 public class VendaCorrente extends Leilao{
+    @OneToOne
+    private Leilao leilao_id;
     private double preco_inicial;
     private double preco_atual;
 }
--- src/main/java/ifsuldeminas/pas/bcc/teii/Etapa2/model/entity/VendaDireta.java
+++ src/main/java/ifsuldeminas/pas/bcc/teii/Etapa2/model/entity/VendaDireta.java
@@ -1,5 +1,12 @@
 package ifsuldeminas.pas.bcc.teii.Etapa2.model.entity;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
+@Entity
+@DiscriminatorValue("2")
 public class VendaDireta extends Leilao{
+    @OneToOne
+    private Leilao leilao_id;
     private double preco_fixo;
 }
